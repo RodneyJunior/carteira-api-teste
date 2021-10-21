a6d3010164f4959f554f6a6a816f34b20697e117
Rodney <perodney@hotmail.com>
2021-10-21 00:10:14 -0300

    mapeamento das entidades transacao e usuario

--- src/main/java/br/com/alura/carteira/modelo/Transacao.java
+++ src/main/java/br/com/alura/carteira/modelo/Transacao.java
@@ -2,7 +2,14 @@ package br.com.alura.carteira.modelo;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,17 +22,26 @@ import lombok.ToString;
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "transacoes")
 public class Transacao {
+	//@Column(name="tck")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String ticker;
 	private BigDecimal preco;
-	private int quantidade;
+	private Integer quantidade;
 	private LocalDate data;
 	
 	//notação do jackson
 	//@JsonIgnore ignora sempre o atributo
-	
+	@Enumerated(EnumType.STRING)
 	private TipoTransacao tipo;
 	
+	@ManyToOne
+	//@JoinColumn(name="id_column")
+	private Usuario usuario;
 /*	
 public Transacao() {}
 
